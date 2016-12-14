977120b9ef22a0d3f385b2fa3772e757f95f54ce
Josephine Chen <jychen@g.hmc.edu>
2016-12-14 14:53:22 -0800

    Fixed minor syntax

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -8,19 +8,17 @@ var Queue = function() {
 
   someInstance.enqueue = function(value) {
     var currentKey = Object.keys(storage).length;
-    storage[currentKey]=value;
+    storage[currentKey] = value;
   };
 
   someInstance.dequeue = function() {
-    var dequeued = storage[0];
     var currentKey = Object.keys(storage).length;
     var temp = storage[currentKey];
     for (var i = 0; i < currentKey - 1; i++) {
       storage[i] = storage[i + 1];
     }
-    storage[currentKey - 1] = temp;
-    delete storage[currentKey - 1];
-    return dequeued;
+    storage[currentKey-1]=temp;
+    delete storage[currentKey-1];
   };
 
   someInstance.size = function() {
