31984d0f7620336452876a8cc9df8726169faa83
Josephine Chen <jychen@g.hmc.edu>
2016-12-14 18:59:20 -0800

    all prototypal queue works

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -11,16 +11,16 @@ var stackMethods = {};
 stackMethods.push = function(value) {
   this.storage[this.counter] = value;
   this.counter++;
-}
+};
 
 stackMethods.pop = function() {
   var temp = this.storage[this.counter - 1];
   delete this.storage[this.counter - 1];
   this.counter--;
   return temp;
-}
+};
 
 stackMethods.size = function() {
   var size = Object.keys(this.storage).length;
   return size;
-}
+};
